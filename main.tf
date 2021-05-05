59f076165233b923f14660692a00280c3f8bddec
Joe Xu <joe.xu28@gmail.com>
2021-05-05 13:33:21 +0800

    changed cidr for subnet

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "aws_vpc" "tfvpc1" {
 }
 
 resource "aws_subnet" "tfsubnet1" {
-    vpc_id = "aws_vpc.tfvpc1.id"
+    vpc_id = aws_vpc.tfvpc1.id
     cidr_block = "10.0.1.0/24"
     tags = {
       "Name" = "prod-subnet1"
